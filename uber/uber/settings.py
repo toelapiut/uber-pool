5ac8c036eb0473233690579896fca2c41ce95412
toelapiut <toelapiut7@gmail.com>
2017-12-04 09:26:21 +0300

    python package

--- uber/uber/settings.py
+++ uber/uber/settings.py
@@ -31,13 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'driver.apps.DriverConfig',
+    'bootstrap3',
+    'tinymce',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
 ]
+
+LOGIN_REDIRECT_URL = ('/')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +83,10 @@ WSGI_APPLICATION = 'uber.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uber',
+        'USER': 'toel',
+        'PASSWORD': 'KingChase',
     }
 }
 
@@ -105,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +128,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+######################### AUTH ############################
+
+# AUTH_USER_MODEL = "driver.CustomUser"
+
+# AUTHENTICATION_BACKENDS = ('driver.backends.CustomUserAuth',)
+
+###########################################################
