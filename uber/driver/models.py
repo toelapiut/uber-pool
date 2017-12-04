8f72e66cd22adfbcfc1ecf95cbdc167af3dfb8c9
toelapiut <toelapiut7@gmail.com>
2017-12-04 09:18:47 +0300

    added  models

--- uber/driver/models.py
+++ uber/driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# class RideRequest(models.Model):
+
+#     ride_id=models.ForeignKey(Rider)
+
+# class car(models.Model):
+#      model=models.CharField(max_length=20)
+#      licence=models.ImageField(upload_to='profpic/', blank=True)
+
+
+#      def __str__(self):
+
+#         return self.model
