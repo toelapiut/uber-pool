490fd9d07310d176ed56e0817ff8c312a487f14a
toelapiut <toelapiut7@gmail.com>
2017-12-04 09:22:16 +0300

    created view function

--- uber/driver/views.py
+++ uber/driver/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from .models import *
+from .forms import *
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth.views import logout
+from django.db import transaction
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    title='Welcome | uber-pool'
+
+    return render(request,'index.html',{'title':title})
+
