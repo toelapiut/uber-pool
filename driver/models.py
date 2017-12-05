9822cda02f2bb01b71857931e677ea8ad1e25e1a
toelapiut <toelapiut7@gmail.com>
2017-12-05 23:40:58 +0300

    Added models  for both rider and the user

--- driver/models.py
+++ driver/models.py
@@ -1,29 +1,80 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.db.models import Q, signals
+from django.dispatch import receiver
 from django.db.models.signals import post_save
-
-
+from phonenumber_field.modelfields import PhoneNumberField
+from uber import settings
 # Create your models here.
 
-class UserProfileManager(models.Manager):
-    def get_queryset(self):
-        return super(UserProfileManager, self).get_queryset().filter(city='London')
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_pic = models.ImageField(upload_to='profile_pictures/', blank=True)
+    car_picture = models.ImageField(upload_to='pictures/')
+    number_plates = models.CharField(max_length=30)
+    capacity = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
+    phone = PhoneNumberField(max_length=30)
+    city = models.CharField(max_length=60)
+    bio = models.CharField(max_length=140, blank=True)
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    description = models.CharField(max_length=100, default='')
-    city = models.CharField(max_length=100, default='')
-    website = models.URLField(default='')
-    phone = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='profile_image', blank=True)
 
-    london = UserProfileManager()
 
-    def __str__(self):
-        return self.user.username
+User.profile = property(
+    lambda u: Driver.objects.get_or_create(user=u)[0])
 
-# def create_profile(sender, **kwargs):
-#     if kwargs['created']:
-#         user_profile = UserProfile.objects.create(user=kwargs['instance'])
 
-# post_save.connect(create_profile, sender=User)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Driver.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+
+# Create your Rider models here.
+
+class Rider(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='rider_profile', unique=True)
+    picture = models.ImageField(upload_to='user_pics/', blank=True)
+    phone = PhoneNumberField(max_length=30)
+    bio = models.CharField(max_length=500, blank=True)
+    username=models.CharField(max_length=12,blank=True)
+    email=models.EmailField()
+
+    @staticmethod
+    def default_riders_group(sender, instance, created, **kwargs):
+        if created and settings.REGISTRATION_DEFAULT_GROUP_NAME:
+            instance.groups.add(Group.objects.get(
+                name=settings.REGISTRATION_DEFAULT_GROUP_NAME))
+
+    @staticmethod
+    def is_rider(instance):
+        return instance.groups.filter(name='riders').exists()
+
+
+post_save.connect(Rider.default_riders_group, sender=User)
+
+
+User.rider_profile = property(
+    lambda u: Rider.objects.get_or_create(user=u)[0])
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Rider.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.rider_profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
