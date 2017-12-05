3d6ce115f93c6cb36d94556df0f4de6a2da75aee
toelapiut <toelapiut7@gmail.com>
2017-12-05 23:40:07 +0300

    added some fields and  to add user information for easier querying

--- driver/forms.py
+++ driver/forms.py
@@ -1,44 +1,27 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-from .models import UserProfile
+from .models import *
 
 
-class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class DriverForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = (
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'password1',
-            'password2'
-        )
-
-    def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.email = self.cleaned_data['email']
-
-        if commit:
-            user.save()
-
-        return user
+        fields = ('first_name', 'last_name', 'email', 'username')
 
 
-class EditProfileForm(UserChangeForm):
-    template_name='/something/else'
+class DriversForm(forms.ModelForm):
 
     class Meta:
+        model = Driver
+        exclude = ['user', 'bio']
+
+
+class RiderForm(forms.ModelForm):
+    class Meta:
         model = User
-        fields = (
-            'email',
-            'first_name',
-            'last_name',
-            'password'
-        )
+        fields = ('first_name', 'last_name', 'email', 'username',)
+
+
+class RiderForm(forms.ModelForm):
+    class Meta:
+        model = Rider
+        fields = ('bio','phone','picture','username', 'email')
